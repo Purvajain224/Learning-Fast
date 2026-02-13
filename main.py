fdd720d24106acfcd486c58a1bd85ddebdf9ab43
Purva <jainpurva38@gamil.com>
2026-02-14 00:03:41 +0530

    10th again

--- main.py
+++ main.py
@@ -75,6 +75,24 @@ async def post_page(request: Request, post_id: int, db: Annotated[AsyncSession, 
         )
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
 
+## login and register template_routes
+@app.get("/login", include_in_schema=False)
+async def login_page(request: Request):
+    return templates.TemplateResponse(
+        request,
+        "login.html",
+        {"title": "Login"},
+    )
+
+
+@app.get("/register", include_in_schema=False)
+async def register_page(request: Request):
+    return templates.TemplateResponse(
+        request,
+        "register.html",
+        {"title": "Register"},
+    )
+
 ## user_posts_page
 @app.get("/users/{user_id}/posts", include_in_schema=False, name="user_posts")
 async def user_posts_page(
